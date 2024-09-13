f9848368f1dbe1ac63f13458b208d5702e2bee32
NolanIle <nolanilisic@gmail.com>
2024-09-13 14:47:11 +0200

    Update main.py and create init_script

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,27 @@
 # backend/app/main.py
 
 import logging
+import app.init_script
+
 from fastapi import FastAPI
 from app.api.wallet_routes import router as wallet_router
+from app.api.transaction_routes import router as transaction_router
+
+# Initialize logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+)
+
+# Overwrite classes
+app.init_script.initialize_overwrite()
 
 # Initialize FastAPI app and logging
 app = FastAPI()
-logging.basicConfig(level=logging.INFO)
 
 # Include router for the wallet-related routes
 app.include_router(wallet_router)
+app.include_router(transaction_router)
 
 
 @app.get("/")
--- backend/app/models/public_key_model.py
+++
@@ -1,6 +0,0 @@
-# backend/app/models/public_key_model.py
-from pydantic import BaseModel
-
-
-class PublicKeyModel(BaseModel):
-    received_public_key: str
