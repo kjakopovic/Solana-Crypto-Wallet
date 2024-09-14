958c68ed302b079a7a88aa6db1e13da0a416cfe1
dini777 <karlojakopov@gmail.com>
2024-09-14 09:01:59 +0200

    project init

---
+++ frontend/constants/icons.js
@@ -0,0 +1,15 @@
+import clearText from '../assets/icons/clear-text.png';
+import exit from '../assets/icons/exit.png';
+import eyeHide from '../assets/icons/eye-hide.png';
+import eye from '../assets/icons/eye.png';
+import menu from '../assets/icons/menu.png';
+import search from '../assets/icons/search.png';
+
+export default {
+  clearText,
+  exit,
+  eyeHide,
+  eye,
+  menu,
+  search,
+};
---
+++ frontend/constants/images.js
@@ -0,0 +1,13 @@
+import lock from "../assets/images/lock.png";
+import logo from "../assets/images/logo.png";
+import welcome1 from "../assets/images/welcome1.png";
+import welcome2 from "../assets/images/welcome2.png";
+import writeDown from "../assets/images/write-down.png";
+
+export default { 
+    lock, 
+    logo, 
+    welcome1, 
+    welcome2, 
+    writeDown
+};
