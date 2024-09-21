20d26740b790e807376347d837bcda59a72e5414
NolanIle <nolanilisic@gmail.com>
2024-09-21 09:09:18 +0200

    Add quiz questions and answers

--- backend/src/config/Database.ts
+++ backend/src/config/Database.ts
@@ -3,6 +3,7 @@
 import { ConnectionPool,  } from 'mssql';
 import dotenv from 'dotenv';
 import logger from './Logger';
+import xlsx from 'xlsx';
 
 dotenv.config();
 const className = 'Database';
@@ -52,7 +53,7 @@ pool.connect()
         logger.error({ message: 'Database connection failed', error: err, className });
     });
 
-export const initializeDatabase = async (pool: ConnectionPool) => {
+const initializeDatabase = async (pool: ConnectionPool) => {
     logger.info('Initializing database', { className });
     try {
         await pool.request().query(`
@@ -78,10 +79,66 @@ export const initializeDatabase = async (pool: ConnectionPool) => {
                 )
             END
         `);
+
+        await pool.request().query(`
+            IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'quizzes')
+            BEGIN
+                CREATE TABLE quizzes (
+                    id INT PRIMARY KEY NOT NULL,
+                    question NVARCHAR(MAX) NOT NULL,
+                    difficulty NVARCHAR(50) NOT NULL,
+                    correctAnswer NVARCHAR(MAX) NOT NULL,
+                    option2 NVARCHAR(MAX) NOT NULL,
+                    option3 NVARCHAR(MAX) NOT NULL,
+                    option4 NVARCHAR(MAX) NOT NULL
+                )
+            END
+        `);
+
+        const result = await pool.request().query(`SELECT COUNT(*) AS count FROM quizzes`);
+        const count = result.recordset[0].count;
+
+        if (count < 90){
+            await populateQuizzesTable(pool);
+        }
+
         logger.info('Database initialized successfully', { className });
     } catch (error) {
         logger.error({ message: 'Error initializing database', error, className });
     }
 };
+
+const populateQuizzesTable = async (pool: ConnectionPool) => {
+    logger.info('Populating quizzes table', { className });
+
+    try{
+        const workbook = xlsx.readFile('../backend/src/data/quizz.xlsx');
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        const questions: any[][] = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+        questions.shift(); // Remove the header row
+
+        for (const row of questions) {
+            const [id, question, difficulty, correctAnswer, option2, option3, option4] = row;
+            await pool.request()
+                .input('id', id)
+                .input('question', question)
+                .input('difficulty', difficulty)
+                .input('correctAnswer', correctAnswer)
+                .input('option2', option2)
+                .input('option3', option3)
+                .input('option4', option4)
+                .query(`
+                    INSERT INTO quizzes (id, question, difficulty, correctAnswer, option2, option3, option4)
+                    VALUES (@id, @question, @difficulty, @correctAnswer, @option2, @option3, @option4)
+                `);
+        }
+
+        logger.info('Quizzes table populated successfully', {className});
+    } catch (error) {
+        logger.error({ message: 'Error populating quizzes table: ' + error, error, className });
+    }
+}
 
 export default pool;
