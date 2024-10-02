3cb29e6cddb0ec7653acc9f95d91bf67ab4dc0bc
NolanIle <nolanilisic@gmail.com>
2024-10-02 18:47:50 +0200

    Update JWT related code

--- backend/src/controllers/JwtController.ts
+++ backend/src/controllers/JwtController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { generateAccessToken, verifyRefreshToken, generateRefreshToken, verifyAccessToken } from '../services/JwtService';
+import JwtService from '../services/JwtService';
 import logger from '../config/Logger';
 import UserService from "../services/UserService";
 
@@ -18,7 +18,7 @@ class JwtController{
         }
 
         try {
-            const decoded = await verifyRefreshToken(refreshToken);
+            const decoded = await JwtService.verifyRefreshToken(refreshToken);
             if(!decoded){
                 logger.error('Refresh token not found in the database', { className });
                 return res.status(403).json({ message: 'Invalid refresh token' });
@@ -26,7 +26,7 @@ class JwtController{
 
             logger.info(`Refresh token verified for user: ${decoded.id}, ${decoded.username}`, { className });
 
-            const accessToken = generateAccessToken({ id: decoded.id, username: decoded.username, publicKey: decoded.publicKey });
+            const accessToken = JwtService.generateAccessToken({ id: decoded.id, username: decoded.username, publicKey: decoded.publicKey });
             logger.info(`Access token generated successfully for user: ${decoded.id}, ${decoded.username}`, { className, accessToken });
 
             return res.status(200).json({ accessToken });
@@ -36,71 +36,6 @@ class JwtController{
         }
     };
 
-    // Controller to create a refresh token
-    public createRefreshTokenController = async (req: Request, res: Response): Promise<Response> => {
-        const { username, publicKey } = req.body;
-        logger.info(`Creating refresh token for user with publicKey: ${publicKey}`, { className });
-
-        if (!publicKey || !username) {
-            logger.error('PublicKey and username are required', { className });
-            return res.status(400).json({ message: 'User ID and username are required' });
-        }
-
-        const user = await UserService.findUserByField("publicKey", publicKey);
-        if(!user){
-            logger.error('User not found', { className });
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        try {
-            const refreshToken = generateRefreshToken({ id: user.id, username, publicKey });
-            logger.info('Refresh token created successfully', { className });
-            return res.status(200).json({ refreshToken });
-        } catch (error) {
-            logger.error({ message: 'Error creating refresh token', error, className });
-            return res.status(500).json({ message: 'Internal server error', error });
-        }
-    };
-
-    // Controller to verify an access token
-    public verifyAccessTokenController = async (req: Request, res: Response): Promise<Response> => {
-        const { accessToken } = req.body;
-        logger.info(`Verifying access token: ${accessToken}`, { className });
-
-        if (!accessToken) {
-            logger.error('Access token is required', { className });
-            return res.status(401).json({ message: 'Access token is required', error: 'Access token is required' });
-        }
-
-        try {
-            const decoded = verifyAccessToken(accessToken);
-            logger.info('Access token is valid', { className });
-            return res.status(200).json({ valid: true, decoded });
-        } catch (error) {
-            logger.error({ message: 'Invalid access token', error, className });
-            return res.status(403).json({ message: 'Invalid access token', error });
-        }
-    }
-
-    // Controller to verify a refresh token
-    public verifyRefreshTokenController = async (req: Request, res: Response): Promise<Response> => {
-        const { refreshToken } = req.body;
-        logger.info(`Verifying refresh token: ${refreshToken}`, { className });
-
-        if (!refreshToken) {
-            logger.error('Refresh token is required', { className });
-            return res.status(401).json({ message: 'Refresh token is required', error: 'Refresh token is required' });
-        }
-
-        try {
-            const decoded = await verifyRefreshToken(refreshToken);
-            logger.info('Refresh token is valid', { className });
-            return res.status(200).json({ valid: true, decoded });
-        } catch (error) {
-            logger.error({ message: 'Invalid refresh token', error, className });
-            return res.status(403).json({ message: 'Invalid refresh token', error });
-        }
-    }
 }
 
 export default new JwtController();
--- backend/src/services/JwtService.ts
+++ backend/src/services/JwtService.ts
@@ -15,59 +15,61 @@ interface UserPayload {
     publicKey: string;
 }
 
-// Generate Access Token
-export const generateAccessToken = (user: UserPayload): string => {
-    try {
-        logger.info('Generating access token for userId: ' + user.id, { className });
-        const token = jwt.sign({ id: user.id, username: user.username }, accessTokenSecret, { expiresIn: '5m' });
-        logger.info('Access token generated successfully: ' + token, { className });
-        return token;
-    } catch (error) {
-        logger.error('Error generating access token: ' + error, { className });
-        throw error;
-    }
-};
+class JwtService{
 
-// Generate Refresh Token
-export const generateRefreshToken = (user: UserPayload): string => {
-    try {
-        logger.info('Generating refresh token for userId: ' + user.id, { className });
-        const token = jwt.sign({ id: user.id, username: user.username, publicKey: user.publicKey }, refreshTokenSecret, { expiresIn: '2h' });
-        logger.info('Refresh token generated successfully: ' + token, { className });
-        return token;
-    } catch (error) {
-        logger.error('Error generating refresh token: ' + error, { className });
-        throw error;
-    }
-};
-
-// Verify Access Token
-export const verifyAccessToken = (token: string): JwtPayload | string => {
-    try {
-        logger.info('Verifying access token: ' + token, { className });
-        return jwt.verify(token, accessTokenSecret);
-    } catch (error) {
-        logger.error('Error verifying access token: ' + error, { className });
-        throw error;
-    }
-};
-
-// Verify Refresh Token
-export const verifyRefreshToken = async (token: string): Promise<UserPayload | null> => {
-    logger.info('Verifying refresh token: ' + token, { className });
-
-    try {
-        const decoded = jwt.verify(token, refreshTokenSecret) as UserPayload;
-        const user = await UserModel.findUserByField("refreshToken", token);
-
-        if (!user) {
-            logger.error('Refresh token not found in the database', { className });
-            return null;
+    public generateAccessToken = (user: UserPayload): string => {
+        try {
+            logger.info('Generating access token for userId: ' + user.id, { className });
+            const token = jwt.sign({ id: user.id, username: user.username }, accessTokenSecret, { expiresIn: '5m' });
+            logger.info('Access token generated successfully: ' + token, { className });
+            return token;
+        } catch (error) {
+            logger.error('Error generating access token: ' + error, { className });
+            throw error;
         }
+    };
 
-        return decoded;
-    } catch (error) {
-        logger.error('Error verifying refresh token: ' + error, { className });
-        throw error;
-    }
-};
+    public generateRefreshToken = (user: UserPayload): string => {
+        try {
+            logger.info('Generating refresh token for userId: ' + user.id, { className });
+            const token = jwt.sign({ id: user.id, username: user.username, publicKey: user.publicKey }, refreshTokenSecret, { expiresIn: '2h' });
+            logger.info('Refresh token generated successfully: ' + token, { className });
+            return token;
+        } catch (error) {
+            logger.error('Error generating refresh token: ' + error, { className });
+            throw error;
+        }
+    };
+
+    public verifyAccessToken = (token: string): JwtPayload | string => {
+        try {
+            logger.info('Verifying access token: ' + token, { className });
+            return jwt.verify(token, accessTokenSecret);
+        } catch (error) {
+            logger.error('Error verifying access token: ' + error, { className });
+            throw error;
+        }
+    };
+
+    public verifyRefreshToken = async (token: string): Promise<UserPayload | null> => {
+        logger.info('Verifying refresh token: ' + token, { className });
+
+        try {
+            const decoded = jwt.verify(token, refreshTokenSecret) as UserPayload;
+            const user = await UserModel.findUserByField("refreshToken", token);
+
+            if (!user) {
+                logger.error('Refresh token not found in the database', { className });
+                return null;
+            }
+
+            return decoded;
+        } catch (error) {
+            logger.error('Error verifying refresh token: ' + error, { className });
+            throw error;
+        }
+    };
+
+}
+
+export default new JwtService();
