c76b2424050d39f7af90e47e85c8b57c93fc6cc7
NolanIle <nolanilisic@gmail.com>
2024-10-02 18:20:14 +0200

    Create ChallengeService.test.ts

--- backend/src/config/database/Database.ts
+++ backend/src/config/database/Database.ts
@@ -5,16 +5,28 @@ import { initializeDatabase } from './Initialize';
 import logger from '../Logger';
 const className = 'Database';
 
-pool.connect()
-    .then(async () => {
-        console.log('Connected to SQL Server');
-        logger.info('Connected to SQL Server', { className });
-        await initializeDatabase(pool);
-        logger.info('Database initialized', { className });
-    })
-    .catch(err => {
-        console.error('Database connection failed:', err);
-        logger.error({ message: 'Database connection failed', error: err, className });
-    });
+if(process.env.NODE_ENV === 'test') {
+    pool.connect()
+        .then(async () => {
+            console.log('Connected to SQL Server');
+            logger.info('Connected to SQL Server', { className });
+        })
+        .catch(err => {
+            console.error('Database connection failed:', err);
+            logger.error({ message: 'Database connection failed', error: err, className });
+        });
+}else{
+    pool.connect()
+        .then(async () => {
+            console.log('Connected to SQL Server');
+            logger.info('Connected to SQL Server', { className });
+            await initializeDatabase(pool);
+            logger.info('Database initialized', { className });
+        })
+        .catch(err => {
+            console.error('Database connection failed:', err);
+            logger.error({ message: 'Database connection failed', error: err, className });
+        });
+}
 
 export default pool;
