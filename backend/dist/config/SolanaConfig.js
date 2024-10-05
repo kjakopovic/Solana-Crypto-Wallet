aca3828f74f60f8d0c0e90f0e04cd6f11f9acf56
NolanIle <nolanilisic@gmail.com>
2024-10-05 16:44:40 +0200

    Remove unnecesary code

--- backend/dist/config/SolanaConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const web3_js_1 = require("@solana/web3.js");
-const connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)('devnet'));
-exports.default = connection;
--- backend/dist/routes/WalletRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const WalletController_1 = require("../controllers/WalletController");
-const router = (0, express_1.Router)();
-router.post('/createWallet', WalletController_1.createWalletController);
-router.get('/getBalance/:publicKey', WalletController_1.getBalanceController);
-router.post('/transferSol', WalletController_1.transferSolController);
-exports.default = router;
