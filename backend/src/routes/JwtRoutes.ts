dbb4bf138f3eef9ea770ea68b5fa4b9a3502f72d
NolanIle <nolanilisic@gmail.com>
2024-10-06 08:58:36 +0200

    Remove unused code

--- backend/src/routes/JwtRoutes.ts
+++ backend/src/routes/JwtRoutes.ts
@@ -5,35 +5,6 @@ import JwtController from '../controllers/JwtController';
 
 const router = Router();
 
-// TODO: Implement middleware to check if the user is logged in
-// TODO: Implement CORS allowed origin on all routes
-/**
- * @swagger
- * /api/v1/jwt/refresh-access:
- *   post:
- *     summary: Refresh access token
- *     description: Generate access token by providing the refresh token
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               refreshToken:
- *                 type: string
- *     responses:
- *       200:
- *         description: Access token refreshed successfully
- *       400:
- *         description: Invalid input
- *       401:
- *         description: Invalid refresh token
- *       500:
- *         description: Error refreshing access token
- */
-router.post('/refresh-access', JwtController.refreshAccessTokenController);
-
 router.post('/verify-refresh', JwtController.verifyRefreshTokenController);
 
 export default router;
